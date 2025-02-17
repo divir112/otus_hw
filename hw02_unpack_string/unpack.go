bb2721baec78dd893f870676dc2a1ba85783e0ce
divir112 <divir19983@mail.ru>
2025-02-17 22:33:50 +0300

    HW2 additional task

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -12,33 +12,53 @@ var ErrInvalidString = errors.New("invalid string")
 func Unpack(str string) (string, error) {
 	var result strings.Builder
 	prevIsDigit := true
+	prevIsShielding := false
 	letters := []rune(str)
 
 	for i := 0; i < len(letters); i++ {
+
+		if !prevIsShielding && letters[i] == '\\' {
+			prevIsShielding = true
+			prevIsDigit = false
+			continue
+		}
+
+		if prevIsShielding && (!unicode.IsDigit(letters[i]) && letters[i] != '\\') {
+			return "", ErrInvalidString
+		}
+
 		if unicode.IsDigit(letters[i]) {
 			if prevIsDigit {
 				return "", ErrInvalidString
 			}
-			prevIsDigit = true
-			continue
+
+			if prevIsShielding {
+				prevIsDigit = false
+			} else {
+				prevIsDigit = true
+				continue
+			}
+
 		}
 
 		if len(letters)-1 == i {
-			if !unicode.IsDigit(letters[i]) {
+			if !unicode.IsDigit(letters[i]) || prevIsShielding {
 				result.WriteRune(letters[i])
 			}
 			break
 		}
 
 		prevIsDigit = false
-		if unicode.IsDigit(letters[i+1]) {
+		if unicode.IsDigit(letters[i+1]) && prevIsShielding || unicode.IsDigit(letters[i+1]) {
 			iterations, _ := strconv.Atoi(string(letters[i+1]))
 			for j := 0; j < iterations; j++ {
 				result.WriteRune(letters[i])
 			}
+
 		} else {
 			result.WriteRune(letters[i])
 		}
+		prevIsShielding = false
 	}
 	return result.String(), nil
 }
