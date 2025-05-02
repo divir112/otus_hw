b1ac418e0d39fdfe10baa5b0c8d796afb6a53213
divir112 <divir19983@mail.ru>
2025-05-02 17:45:24 +0300

    HW10 fix

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -2,7 +2,6 @@ package hw10programoptimization
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 	"strings"
 )
@@ -20,39 +19,23 @@ type User struct {
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
-}
-
-type users []User
-
-func getUsers(r io.Reader) (result users, err error) {
 	scanner := bufio.NewScanner(r)
+
+	result := make(DomainStat)
+	substr := "." + domain
 	for scanner.Scan() {
 		var user User
 		line := scanner.Bytes()
-		if err = user.UnmarshalJSON(line); err != nil {
-			return
+
+		if err := user.UnmarshalJSON(line); err != nil {
+			return nil, err
 		}
-		result = append(result, user)
-	}
-
-	return
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
-	result := make(DomainStat)
-
-	substr := "." + domain
-	for _, user := range u {
 
 		if strings.Contains(user.Email, substr) {
 			key := strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])
 			result[key]++
 		}
 	}
+
 	return result, nil
 }
